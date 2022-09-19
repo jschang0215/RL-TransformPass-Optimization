068828ddcf54d425e29ce5bca6bfb39b8dc8333d
JSChang <jschang0215@naver.com>
2022-09-19 11:47:38 +0900

    Experiment Data

--- Experiment_Result/Parse_Data.py
+++ Experiment_Result/Parse_Data.py
@@ -22,8 +22,10 @@ os_memory_reduction_list = []
 os_total_time_list = []
 os_time_reduction_list = []
 
-memory_result = []
-time_result = []
+memory_percent_result = []
+memory_value_result = []
+time_percent_result = []
+time_value_result = []
 with open(os_FILE, newline='') as f:
     reader = csv.reader(f)
     base_data = list(reader)
@@ -47,9 +49,10 @@ with open(os_FILE, newline='') as f:
         diff = (os_time_reduction_list[idx] / raw_total_time_list[idx]) * 100
         base_time_reduction_percent_list.append(diff)
 
-memory_result.append(base_memory_reduction_percent_list)
-time_result.append(base_time_reduction_percent_list)
-
+memory_percent_result.append(base_memory_reduction_percent_list)
+memory_value_result.append(os_memory_reduction_list)
+time_percent_result.append(base_time_reduction_percent_list)
+time_value_result.append(os_total_time_list)
 
 o3_total_memory_list = []
 o3_memory_reduction_list = []
@@ -85,34 +88,44 @@ for result_file in RESULT_FILE_LIST:
     for idx in range(len(memory_reduction_list)):
         diff = (memory_reduction_list[idx] / raw_total_memory_list[idx]) * 100
         memory_reduction_precent_list.append(diff)
-    memory_result.append(memory_reduction_precent_list)
+    memory_percent_result.append(memory_reduction_precent_list)
+    memory_value_result.append(memory_reduction_list)
 
     time_reduction_list = []
+    time_list = []
     for idx in range(len(data)):
         time_value = data[idx][2]*os_total_time_list[idx]/100+os_total_time_list[idx]
         diff = -(time_value-raw_total_time_list[idx])
         time_reduction_list.append(diff)
+        time_list.append(time_value)
     time_reduction_percent_list = []
     for idx in range(len(time_reduction_list)):
         diff = (time_reduction_list[idx] / raw_total_time_list[idx]) * 100
         time_reduction_percent_list.append(diff)
-    time_result.append(time_reduction_percent_list)
+    time_percent_result.append(time_reduction_percent_list)
+    time_value_result.append(time_list)
     
 print("\nMemory Average")
-memory_average_percent = list(map(statistics.mean, memory_result))
+memory_average_percent = list(map(statistics.mean, memory_percent_result))
+memory_average_value = list(map(statistics.mean, memory_value_result))
+print("-os Memory reduced (byte)", memory_average_value[0])
 for idx in range(1, len(memory_average_percent)):
+    print(RESULT_FILE_LIST[idx-1].split('/')[3], "Memory reduced (byte)", memory_average_value[idx])
     print(RESULT_FILE_LIST[idx-1].split('/')[3], "Improve compare to -os: ", 
-    round((memory_average_percent[idx]-memory_average_percent[0])/memory_average_percent[0]*100, 3), "%")
+    round((memory_average_value[idx]-memory_average_value[0])/memory_average_value[0]*100, 3), "%")
 
 print("\nTime Average")
-time_average_percent = list(map(statistics.mean, time_result))
+time_average_percent = list(map(statistics.mean, time_percent_result))
+time_average_value = list(map(statistics.mean, time_value_result))
+print("-os Time (ms)", time_average_value[0]*1000)
 for idx in range(1, len(time_average_percent)):
+    print(RESULT_FILE_LIST[idx-1].split('/')[3], "Time (ms)", time_average_value[idx]*1000)
     print(RESULT_FILE_LIST[idx-1].split('/')[3], "Improve compare to -os: ", 
-    round((time_average_percent[idx]-time_average_percent[0])/time_average_percent[0]*100, 3), "%")
+    round((time_average_value[idx]-time_average_value[0])/time_average_value[0]*100, 3), "%")
 
-memory_result = list(map(list, zip(*memory_result)))
+memory_percent_result = list(map(list, zip(*memory_percent_result)))
 with open("./Memory_Result.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerow([os_FILE] + RESULT_FILE_LIST)
-    for idx in range(1, len(memory_result)):
-        writer.writerow(memory_result[idx])
+    for idx in range(1, len(memory_percent_result)):
+        writer.writerow(memory_percent_result[idx])
